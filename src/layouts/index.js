c4186d237831d014cee6a4cce7d97befc90733c2
robabby <robabby23@gmail.com>
2018-01-13 12:30:59 -0600

    Refactored layout to Class component

--- src/layouts/index.js
+++ src/layouts/index.js
@@ -1,35 +1,48 @@
 import React from 'react'
-import PropTypes from 'prop-types'
 import Helmet from 'react-helmet'
+import get from 'lodash/get'
+
+import { rhythm, scale } from '../utils/typography'
 
 import Header from '../components/Header'
 import '../assets/scss/app.scss'
 
-const TemplateWrapper = ({ children }) => (
-  <div>
-    <Helmet
-      title="Gatsby Default Starter"
-      meta={[
-        { name: 'description', content: 'Sample' },
-        { name: 'keywords', content: 'sample, something' },
-      ]}
-    />
-    <Header />
-    <div
-      style={{
-        margin: '0 auto',
-        maxWidth: 960,
-        padding: '0px 1.0875rem 1.45rem',
-        paddingTop: 0,
-      }}
-    >
-      {children()}
-    </div>
-  </div>
-)
+class Template extends React.Component {
+  render() {
+    const { location, children } = get(this, 'props')
 
-TemplateWrapper.propTypes = {
-  children: PropTypes.func,
+    let rootPath = `/`
+    let isRoot = location.pathname === rootPath
+
+    if (typeof __PREFIX_PATHS__ !== `undefined` && __PREFIX_PATHS__) {
+      rootPath = __PATH_PREFIX__ + `/`
+    }
+
+    return (
+      <div>
+        <Helmet
+          title="Gatsby Default Starter"
+          meta={[
+            { name: 'description', content: 'Sample' },
+            { name: 'keywords', content: 'sample, something' },
+          ]}
+        />
+        <Header />
+        <div
+          style={{
+            margin: '0 auto',
+            maxWidth: 960,
+            padding: '0px 1.0875rem 1.45rem',
+            paddingTop: 0,
+          }}
+        >
+          {children()}
+        </div>
+      </div>
+    )
+  }
 }
 
-export default TemplateWrapper
+
+
+export default Template
