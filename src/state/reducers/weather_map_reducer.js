76d8e55f5092288c449faf96d80bbb5e157ec1b3
robabby <robabby23@gmail.com>
2018-01-14 11:54:39 -0600

    Cleanup

--- src/state/reducers/weather_map_reducer.js
+++ src/state/reducers/weather_map_reducer.js
@@ -5,9 +5,6 @@ import {
 export default (state = {}, action) => {
   switch (action.type) {
     case GET_WEATHER_MAP:
-
-      console.log("GET_WEATHER_MAP | action.payload", action.payload);
-
       return {
         image: action.payload
       };
